e8ef66c0e81f7d604a1047d24e55dd5b0ebb0b95
mbarrosos <mbarrosos@hotmail.com>
2021-11-25 11:01:59 -0500

    Se quita hello world

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //cargar rutas
-const hello_routes = require("./routers/hello");
 const task_routes = require("./routers/task");
 
 //Rutas bases
-app.use("/api", hello_routes);
 app.use("/api", task_routes);
 
 module.exports = app;
--- controllers/hello.js
+++
@@ -1,9 +0,0 @@
-function getHello(req, res){
-    res.status(200).send({
-        msg: "Hola putito!",
-    })
-}
-
-module.exports = {
-    getHello,
-}
